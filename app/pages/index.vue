41efbb3ca44fc335a40920b510a3b21d74045d5d
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-14 20:47:57 -0400

    feat: create mockup layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <ClientOnly><Canvas /></ClientOnly>
+  <div class="flex h-screen">
+    <div class="flex flex-1 flex-col gap-2 p-2">
+      <UCard>
+        <Logo />
+      </UCard>
+      <UCard class="size-full">
+        Test
+      </UCard>
+    </div>
+
+    <div class="flex h-full w-[1450px] shrink-0 items-center justify-center">
+      <ClientOnly>
+        <Canvas />
+      </ClientOnly>
+    </div>
+
+    <div class="flex-1 p-2">
+      <UCard class="size-full">
+        Test
+      </UCard>
+    </div>
+  </div>
 </template>
