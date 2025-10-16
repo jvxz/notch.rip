034273aa95abc27cb1171d4140675d89e78c0a14
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-16 16:31:40 -0400

    feat: add CenterPanel component

---
+++ app/components/center-panel.vue
@@ -0,0 +1,9 @@
+<template>
+  <UCard class="relative flex h-full w-[1450px] shrink-0 items-center justify-center overflow-hidden">
+    <Transition name="zoom">
+      <Canvas />
+    </Transition>
+
+    <CenterPanelGrid />
+  </UCard>
+</template>
