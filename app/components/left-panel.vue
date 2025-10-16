a79dbdda2a8998bc5ff8032100cfce56c6a4136b
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-16 16:25:48 -0400

    feat: create LeftPanel component

---
+++ app/components/left-panel.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const { imageFile } = useImageInput()
+
+const { onChange: onFileChange, open: openFileDialog } = useFileDialog({
+  accept: 'image/png, image/jpeg',
+  multiple: false,
+})
+
+onFileChange((files) => {
+  if (files && files[0]) {
+    imageFile.value = files[0]
+  }
+})
+</script>
+
+<template>
+  <UCard class="size-full">
+    <div class="flex gap-2 *:flex-1">
+      <UButton @click="openFileDialog">
+        Import image
+      </UButton>
+      <UButton :disabled="!imageFile" @click="imageFile = null">
+        Clear image
+      </UButton>
+    </div>
+  </UCard>
+</template>
