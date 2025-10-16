1e100837eca14037411525a412483b990f9553ca
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-16 16:25:56 -0400

    feat: create RightPanel component

---
+++ app/components/right-panel.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { triggerCanvasDownload } = useCanvas()
+</script>
+
+<template>
+  <UCard class="size-full">
+    <UButton @click="triggerCanvasDownload">
+      Download
+    </UButton>
+  </UCard>
+</template>
