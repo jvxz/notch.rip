bf4da9257ac1f2bae97f0ac52ee59366e23e61f6
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-18 14:13:30 -0400

    refactor: edit right panel component

--- app/components/right-panel.vue
+++ app/components/right-panel.vue
@@ -3,7 +3,11 @@ const { triggerCanvasDownload } = useCanvas()
 </script>
 
 <template>
-  <UCard class="size-full">
+  <UCard class="size-full flex-col gap-2">
+    <h1 class="mb-2 text-2xl font-medium">
+      Bar & corners
+    </h1>
+    <RightPanelPresetSelect />
     <UButton @click="triggerCanvasDownload">
       Download
     </UButton>
