773d04cc7b365dc83e1c3bc86b93b0fdc8db5cd3
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-18 14:12:12 -0400

    refactor: edit useCanvas composable to use useConfig composable

--- app/composables/use-canvas.ts
+++ app/composables/use-canvas.ts
@@ -3,19 +3,23 @@ const canvasDownloadEvent = createEventHook()
 export const useCanvas = createSharedComposable(() => {
   const { width: windowWidth } = useWindowSize()
   const ssrWidth = useSSRWidth()
+  const { config } = useConfig()
 
   const canvasSize = computed(() => {
     const width = ssrWidth ?? windowWidth.value
 
-    if (width > 1536) {
-      return { height: 1080 / 2, width: 1662.89 / 2 }
+    if (width >= 2278) {
+      const h = 1080 / 1.7
+      return { height: h, width: h * config.resolution.value }
     }
-
-    if (width < 1280) {
-      return { height: 1080 / 3, width: 1662.89 / 3 }
+    if (width >= 1690) {
+      const h = 1080 / 2
+      return { height: h, width: h * config.resolution.value }
     }
-
-    return { height: 1080 / 4, width: 1662.89 / 4 }
+    else {
+      const h = 1080 / 3
+      return { height: h, width: h * config.resolution.value }
+    }
   })
 
   return {
