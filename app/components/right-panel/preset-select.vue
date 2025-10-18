7e425921c2721c731be7413c76976dceba9872b7
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-18 14:12:30 -0400

    feat: create preset select component

---
+++ app/components/right-panel/preset-select.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const { applyPreset, currentPreset } = useConfig()
+</script>
+
+<template>
+  <div class="flex flex-1 flex-col gap-2">
+    <p>Presets</p>
+    <USelectRoot @update:model-value="(value) => applyPreset(value as PresetKey)">
+      <USelectTrigger class="w-full text-muted-foreground">
+        {{ currentPreset }}
+      </USelectTrigger>
+      <USelectContent>
+        <USelectItem
+          v-for="preset in PRESET_KEYS"
+          :key="preset"
+          :value="preset"
+        >
+          <USelectItemText>{{ preset }}</USelectItemText>
+        </USelectItem>
+      </USelectContent>
+    </USelectRoot>
+  </div>
+</template>
