9a018c68d9bccfd1d77b9ec131aef2aca05e846e
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-20 17:25:06 -0400

    feat: add get screen aspect button to aspect controls

--- app/components/right-panel/aspect-controls.vue
+++ app/components/right-panel/aspect-controls.vue
@@ -1,10 +1,24 @@
 <script lang="ts" setup>
 const config = useConfigValues()
+
+function getScreenAspect() {
+  config.aspect.value.height = screen.height
+  config.aspect.value.width = screen.width
+}
 </script>
 
 <template>
   <div class="flex flex-col gap-2">
-    <p>Aspect ratio</p>
+    <div class="flex items-center justify-between gap-2">
+      <p>Aspect ratio</p>
+      <UButton
+        size="sm"
+        variant="soft"
+        @click="getScreenAspect"
+      >
+        Get screen aspect
+      </UButton>
+    </div>
     <div class="flex items-center gap-2">
       <UNumberFieldRoot v-model:model-value="config.aspect.value.width" class="flex-1">
         <UNumberFieldContent>
