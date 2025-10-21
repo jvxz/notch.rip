51e1b30176db518673029b6f41daae5af1e4aff8
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-10-21 15:39:42 -0400

    feat: add github link to header

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="flex h-screen p-2 gap-2">
+  <div class="flex h-screen gap-2 p-2">
     <div class="flex flex-1 flex-col gap-2">
       <UCard class="flex-row justify-between">
         <div class="flex items-center gap-2">
           <Logo /> <span class="text-2xl font-medium">notch.rip</span>
         </div>
-        <UButton
-          variant="ghost"
-          size="icon"
-          @click="toggleColorMode"
-        >
-          <Icon :name="$colorMode.preference === 'dark' ? 'tabler:sun' : 'tabler:moon'" />
-        </UButton>
+        <div class="flex items-center gap-2">
+          <UButton
+            variant="ghost"
+            size="icon"
+            class="cursor-default"
+            as-child
+          >
+            <NuxtLink to="https://github.com/jvxz/notch.rip" target="_blank">
+              <Icon name="tabler:brand-github" />
+            </NuxtLink>
+          </UButton>
+          <UButton
+            variant="ghost"
+            size="icon"
+            @click="toggleColorMode"
+          >
+            <Icon :name="$colorMode.value === 'dark' ? 'tabler:sun' : 'tabler:moon'" />
+          </UButton>
+        </div>
       </UCard>
       <LeftPanel />
     </div>
-
     <CenterPanel />
-
-    <!-- <div class="flex-1">
-      <RightPanel />
-    </div> -->
   </div>
 </template>
