f1a0d1cefd8a7037227131f057276a214cc8a1e5
Jamie <hi@jamie.to>
2026-08-21 13:24:47 -0400

    feat: improve presets

--- app/composables/use-config.ts
+++ app/composables/use-config.ts
@@ -13,9 +13,9 @@ export interface Config {
   notch: boolean
 }
 export type PresetKey = typeof PRESET_KEYS[number]
-export const PRESET_KEYS = ['MacBook', 'Desktop'] as const
+export const PRESET_KEYS = ['MacBook (MacOS 15≤)', 'MacBook (MacOS 26+)', 'Desktop (MacOS 15≤)', 'Desktop (MacOS 26+)'] as const
 export const PRESETS: Record<PresetKey, Config> = {
-  Desktop: {
+  'Desktop (MacOS 15≤)': {
     aspect: {
       height: 9,
       width: 16,
@@ -29,7 +29,21 @@ export const PRESETS: Record<PresetKey, Config> = {
     menubarHeightScale: 0.0165,
     notch: false,
   },
-  MacBook: {
+  'Desktop (MacOS 26+)': {
+    aspect: {
+      height: 9,
+      width: 16,
+    },
+    corners: {
+      bl: true,
+      br: true,
+      tl: true,
+      tr: true,
+    },
+    menubarHeightScale: 0.02,
+    notch: false,
+  },
+  'MacBook (MacOS 15≤)': {
     aspect: {
       height: 1964,
       width: 3024,
@@ -43,20 +57,34 @@ export const PRESETS: Record<PresetKey, Config> = {
     menubarHeightScale: 0.039,
     notch: true,
   },
+  'MacBook (MacOS 26+)': {
+    aspect: {
+      height: 1964,
+      width: 3024,
+    },
+    corners: {
+      bl: true,
+      br: true,
+      tl: true,
+      tr: true,
+    },
+    menubarHeightScale: 0.031,
+    notch: true,
+  },
 } as const
 
 export function useConfigValues() {
   const corners = useCookie('config:corners', {
-    default: () => ({ ...PRESETS.MacBook.corners }),
+    default: () => ({ ...PRESETS['MacBook (MacOS 26+)'].corners }),
   })
   const menubarHeightScale = useCookie('config:menubar-height-scale', {
-    default: () => PRESETS.MacBook.menubarHeightScale,
+    default: () => PRESETS['MacBook (MacOS 26+)'].menubarHeightScale,
   })
   const notch = useCookie('config:notch', {
-    default: () => PRESETS.MacBook.notch,
+    default: () => PRESETS['MacBook (MacOS 26+)'].notch,
   })
   const aspect = useCookie('config:aspect', {
-    default: () => ({ ...PRESETS.MacBook.aspect }),
+    default: () => ({ ...PRESETS['MacBook (MacOS 26+)'].aspect }),
   })
   const cornersColor = useCookie('config:corners-color', {
     default: () => '#000000',
@@ -81,7 +109,7 @@ export function useConfigValues() {
 
 export const useConfig = createSharedComposable(() => {
   const config = useConfigValues()
-  const currentPreset = ref<PresetKey | 'Custom'>('MacBook')
+  const currentPreset = ref<PresetKey | 'Custom'>('MacBook (MacOS 26+)')
 
   async function applyPreset(preset: PresetKey) {
     config.aspect.value.height = PRESETS[preset].aspect.height
